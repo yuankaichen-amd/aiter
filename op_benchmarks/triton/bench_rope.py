01a8676f00d6f581f4b6c08eb4e276fc79a97b54
k50112113 <Shao-Chun.Lee@amd.com>
2025-05-06 10:07:31 -0500

    Triton rope (#380)
    * fix bugs
    
    * fix bug for bench_rope

--- op_benchmarks/triton/bench_rope.py
+++ op_benchmarks/triton/bench_rope.py
@@ -44,6 +44,14 @@ def generate_rope_inputs(B: int, S: int, H: int, D: int, cached: bool, reuse_fre
 
     return x, y, freqs, positions, offsets, cos, sin
 
+def str_to_bool(v, vstr):
+    if v.lower() in ['true', 'yes']:
+        return True
+    elif v.lower() in ['false', 'no']:
+        return False
+    else:
+        raise NotImplementedError(f"invalid {vstr}: {v}")
+
 def get_x_vals():
     """
         this get_x_vals is for DeepSeekV2 (thd)
@@ -73,35 +81,42 @@ def get_x_vals():
     return x_vals
 
 def run_benchmark(args):
-    assert not(args.shape and args.model) or not(args.shape and args.M), \
-        "User can specify --shape or --model MODEL -M VAL exclusively"
+    B, S, H, D, cached, rotate_style, reuse_freqs_front_part, nope, nope_first, pos, offs, two_inputs, layout, inplace, dtype =\
+    args.B, args.S, args.H, args.D, args.cached, args.rotate_style, args.reuse_freqs_front_part, args.nope, args.nope_first, args.pos, args.offs, args.two_inputs, args.l, args.inplace, args.dtype
+    
+    cached = str_to_bool(cached, "cached")
+    reuse_freqs_front_part = str_to_bool(reuse_freqs_front_part, "reuse_freqs_front_part")
+    nope = str_to_bool(nope, "nope")
+    nope_first = str_to_bool(nope_first, "nope_first")
+    pos = str_to_bool(pos, "pos")
+    offs = str_to_bool(offs, "offs")
+    two_inputs = str_to_bool(two_inputs, "two_inputs")
+    inplace = str_to_bool(inplace, "inplace")
+    
+    rep = args.repeat
+
+    if dtype == 'fp16':
+        dtype = torch.float16
+    elif dtype == 'bf16':
+        dtype = torch.bfloat16
+    else:
+        raise NotImplementedError(f"dtype {dtype} not supported")
+    
+    if rotate_style not in ['gptj', 'neox']:
+        raise NotImplementedError(f"rotate_style {rotate_style} not supported")
+        
 
     x_names = ['B', 'S', 'H', 'D', "cached", "rotate_style", "reuse_freqs_front_part", "nope", "nope_first", "pos", "offs", "two_inputs", "layout", "inplace", "dtype"]
-    if args.shape:
-        x_vals_list = [args.shape]
-    else:
-        x_vals_list = get_x_vals()
+    x_vals_list = [(B, S, H, D, cached, rotate_style, reuse_freqs_front_part, nope, nope_first, pos, offs, two_inputs, layout, inplace, dtype)]
+    # x_vals_list = get_x_vals()
 
-    if args.metric == 'time':
-        ylabel = 'Time (ms)'
-    elif args.metric == 'throughput':
-        ylabel = 'Throughput (TFLOPS)'
-    elif args.metric == 'bandwidth':
-        ylabel = 'Bandwidth (GB/s)'
-    else:
-        raise NotImplementedError(f"{args.metric} is not supported")
-
-    line_names = ["Triton"]
-    line_vals = ['triton']
-    benchmark = triton.testing.Benchmark(
-        x_names=x_names, x_vals=x_vals_list,
-        line_arg='provider', line_vals=line_vals, line_names=line_names,
-        styles=[('green', '-')],
-        ylabel=ylabel, plot_name=f'RoPE Benchmark', args={"metric": args.metric})
-
-    @triton.testing.perf_report([benchmark])
+    # @triton.testing.perf_report([benchmark])
     def bench_rope(B: int, S: int, H: int, D: int, cached: bool, rotate_style: int, reuse_freqs_front_part: bool, nope: bool, nope_first: bool, pos: bool, offs: bool, two_inputs: bool, layout: str, inplace: bool, dtype: torch.dtype, metric, provider):
         x, y, freqs, positions, offsets, cos, sin = generate_rope_inputs(B, S, H, D, cached, reuse_freqs_front_part, nope, pos, offs, two_inputs, layout, dtype)
+        if rotate_style == 'gptj':
+            rotate_style = RotateStyle.GPTJ
+        elif rotate_style == 'neox':
+            rotate_style = RotateStyle.NEOX
         # flops
         flops = B * S * H * (D/2.0) * 3.0 * 2.0 * (2.0 if two_inputs else 1.0)
         
@@ -136,47 +151,71 @@ def run_benchmark(args):
         from triton.testing import runtime
         di = runtime.driver.active.get_device_interface()
         cache = runtime.driver.active.get_empty_cache_for_benchmark()
-        for i in range(1000):
+        for i in range(rep):
             cache.zero_()
             di.synchronize()
             fn()
             di.synchronize()
-        ms = 0
+
+        return flops, mem
+    
         # ms = triton.testing.do_bench(fn, warmup=25, rep=100)
+        # bandwidth = mem / (ms * 1e-3) * 1e-9  # GB/s
+        # return bandwidth
 
-        # Return exactly one scalar depending on which metric is active
-        if metric == 'time':
-            return ms
-        elif metric == 'throughput':
-            tflops = flops / ms * 1e-9
-            return tflops
-        elif metric == 'bandwidth':
-            bandwidth = mem / (ms * 1e-3) * 1e-9  # GB/s
-            # print(S, ms*1000, mem/1e9)
-            return bandwidth
-        else:
-            raise ValueError("Unknown metric: " + metric)
-
-
-    bench_rope.run(save_path=".", print_data=True)
+    for x_vals in x_vals_list:
+        print("Running input config:")
+        for i in range(len(x_names)):
+            print(f"    {x_names[i]:23s}= {x_vals_list[0][i]}")
+        print(f"Number of repitition = {rep}")
+        flops, mem = bench_rope(*x_vals, None, None)
+        print(f"Total flops  = {flops/1e12 : .6e} (TFLOPS)")
+        print(f"Total memory = {mem/1e9 : .6e} (GB)")
+    # bench_rope.run(save_path=".", print_data=False)
+    print("")
+    print("This script will not print out runtime as short running kernels cannot be measured accuratly throught triton.testing.do_bench function, please use rocprof to measure accurate runtime, use -h/--help for more information")
 
 
 def parse_args():
     parser = argparse.ArgumentParser(
         prog="Benchmark RoPE",
+        description="This script will not print out runtime as short running kernels cannot be measured accuratly throught triton.testing.do_bench function, please use rocprof to measure accurate runtime (the DurationNs column) in results.csv or results.stats.csv. For instance, try \"rocprof --stats python bench_rope.py -l 'thd' -T 1 -H 128 -D 64 --two_inputs=true\"",
         allow_abbrev=False,
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
-    # available_models = get_available_models()  # Dynamically load model names
-    # model_help = ("Model name to benchmark. Select from: [" + ", ".join(available_models) +
-    #         "]. Use 'all' to benchmark all models or leave blank for the default benchmark script.")
-    # parser.add_argument('--model-configs', type=str, default="utils/model_configs.json",
-    #         help="Model config json file.")
-    # parser.add_argument('--model', type=str, help=model_help)
-    parser.add_argument("--shape", type=int, nargs=3, metavar=("B", "S", "H", "D"),
-            help="user-defined shape to benchmark")
-    parser.add_argument("--metric", type=str, choices=["time", "throughput", "ban-dwidth"],
-            default="bandwidth", help="metric to plot")
+    parser.add_argument("-l", type=str, 
+                        help="'thd' or 'sbhd' the layout of the input.", default='thd')
+    parser.add_argument("-B", type=int,
+                        help="the batch size, B (this argument will be ignored if you set -l to 'thd').", default=1)
+    parser.add_argument("-S", "-T", type=int,
+                        help="the sequence length, S, or the number of tokens, T", default=4096)
+    parser.add_argument("-H", type=int,
+                        help="the number of heads, H", default=128)
+    parser.add_argument("-D", type=int,
+                        help="the head dimension, D", default=64)
+    parser.add_argument("--cached", type=str,
+                        help="cached sin/cos", default='true')
+    parser.add_argument("--rotate_style", type=str,
+                        help="gptj or neox", default='gptj')
+    parser.add_argument("--reuse_freqs_front_part", type=str,
+                        help="turn on reuse_freqs_front_part", default='true')
+    parser.add_argument("--nope", type=str,
+                        help="turn on nope", default='true')
+    parser.add_argument("--nope_first", type=str,
+                        help="turn on nope_fist", default='true')
+    parser.add_argument("--pos", type=str,
+                        help="input positions", default='true')
+    parser.add_argument("--offs", type=str,
+                        help="input offsets", default='false')
+    parser.add_argument("--two_inputs", type=str,
+                        help="input both K and Q", default='true')
+    parser.add_argument("--inplace", type=str,
+                        help="inplace operation", default='false')
+    parser.add_argument("--dtype", type=str,
+                        help="data type", default='bf16')
+    parser.add_argument("--repeat", type=int,
+                        help="number of repetition for benchmark", default=1000)
+    
     args = parser.parse_args()
     return args
 

3f268188a749c3f42503e3aef3a47c5782cb232d
lucas-santos-amd <Lucas.Santos@amd.com>
2025-07-23 17:09:48 -0300

    Add LayerNorm Backward Triton Kernels (#546)

--- aiter/ops/triton/norm.py
+++ aiter/ops/triton/norm.py
@@ -1,13 +1,11 @@
 # SPDX-License-Identifier: MIT
 # Copyright (C) 2024-2025, Advanced Micro Devices, Inc. All rights reserved.
 
-# SPDX-License-Identifier: MIT
-# Copyright (C) 2024-2025, Advanced Micro Devices, Inc. All rights reserved.
-
 import torch
 import triton
 import triton.language as tl
 from typing import Optional
+from aiter.ops.triton.utils.types import get_dtype_max
 
 
 @triton.jit
@@ -37,6 +35,8 @@ def _layernorm_kernel(
     y_ptr,
     w_ptr,
     b_ptr,
+    mean_ptr,
+    rstd_ptr,
     # The stride variables represent how much to increase the ptr by when
     # moving by 1 element in a particular dimension. E.g. `x_row_stride` is
     # how much to increase `x_ptr` by to get the element one row down.
@@ -106,6 +106,10 @@ def _layernorm_kernel(
     var = tl.sum(_var, axis=0) / n_cols
     rstd = tl.rsqrt(var + eps)
 
+    # Write mean / rstd
+    tl.store(mean_ptr + row, mean)
+    tl.store(rstd_ptr + row, rstd)
+
     # Normalize and store
     loop_num_l = loop_num
     for b in range(0, loop_num_l):
@@ -137,6 +141,8 @@ def _fused_add_layernorm_kernel(
     res_out_ptr,
     w_ptr,
     b_ptr,
+    mean_ptr,
+    rstd_ptr,
     # The stride variables represent how much to increase the ptr by when
     # moving by 1 element in a particular dimension. E.g. `x_row_stride` is
     # how much to increase `x_ptr` by to get the element one row down.
@@ -220,6 +226,10 @@ def _fused_add_layernorm_kernel(
 
     var = tl.sum(_var, axis=0) / n_cols
     rstd = tl.rsqrt(var + eps)
+
+    # Write mean / rstd
+    tl.store(mean_ptr + row, mean)
+    tl.store(rstd_ptr + row, rstd)
 
     # Normalize and store
     loop_num_l = loop_num
@@ -571,11 +581,470 @@ def _quant_fused_add_layernorm_kernel(
     tl.store(y_ptr_start + col_offsets, y_block.to(y_ptr.type.element_ty), mask=mask)
 
 
-def get_dtype_max(dtype):
-    if torch.is_floating_point(torch.tensor([], dtype=dtype)):
-        return torch.finfo(dtype).max
+def _layernorm_forward(
+    y: torch.Tensor,
+    x: torch.Tensor,
+    weight: torch.Tensor,
+    bias: torch.Tensor,
+    mean: torch.Tensor,
+    rstd: torch.Tensor,
+    eps: float = 1e-5,
+):
+
+    M, N = x.shape
+
+    # Less than 64KB per feature: enqueue fused kernel
+    MAX_FUSED_SIZE = 65536 // x.element_size()
+    BLOCK_SIZE = min(MAX_FUSED_SIZE, triton.next_power_of_2(N))
+
+    _layernorm_kernel[(M,)](
+        x, y, weight, bias, mean, rstd, x.stride(0), y.stride(0), M, N, eps, BLOCK_SIZE
+    )
+
+    return
+
+
+def _layernorm_forward_with_add(
+    y: torch.Tensor,
+    x: torch.Tensor,
+    res_in: torch.Tensor,
+    res_out: torch.Tensor,
+    weight: torch.Tensor,
+    bias: torch.Tensor,
+    mean: torch.Tensor,
+    rstd: torch.Tensor,
+    epsilon: float,
+):
+
+    M, N = x.shape
+
+    # Less than 64KB per feature: enqueue fused kernel
+    MAX_FUSED_SIZE = 65536 // x.element_size()
+    BLOCK_SIZE = min(MAX_FUSED_SIZE, triton.next_power_of_2(N))
+
+    _fused_add_layernorm_kernel[(M,)](
+        x,
+        y,
+        res_in,
+        res_out,
+        weight,
+        bias,
+        mean,
+        rstd,
+        x.stride(0),
+        y.stride(0),
+        M,
+        N,
+        epsilon,
+        BLOCK_SIZE,
+    )
+
+    return
+
+
+@triton.jit
+def _layernorm_bwd_dx_fused_triton(
+    DX,  # pointer to the input gradient
+    DY,  # pointer to the output gradient
+    DW,  # pointer to the partial sum of weights gradient
+    DB,  # pointer to the partial sum of biases gradient
+    X,  # pointer to the input
+    W,  # pointer to the weights
+    Mean,  # pointer to the mean
+    Rstd,  # pointer to the 1/std
+    stride,  # how much to increase the pointer when moving by 1 row
+    N,  # number of columns in X
+    NUM_ROWS: tl.constexpr,
+    BLOCK_SIZE_N: tl.constexpr,
+    USE_BLOCKED: tl.constexpr,
+    IGNORE_DW_DB: tl.constexpr = False,
+):
+    # Map the program id to the elements of X, DX, and DY it should compute.
+    pid = tl.program_id(0)
+    tile_num = tl.num_programs(0)
+    rows_per_tile = NUM_ROWS // tile_num
+    if pid < NUM_ROWS % tile_num:
+        rows_per_tile += 1
+
+    if USE_BLOCKED:
+        # Blocked approach:
+
+        col_offsets = tl.arange(0, BLOCK_SIZE_N)
+        num_col_blocks = tl.cdiv(N, BLOCK_SIZE_N) - 1
+        row = pid
+
+        for _ in range(0, rows_per_tile):
+            # Load row statistics:
+            mean = tl.load(Mean + row)
+            rstd = tl.load(Rstd + row)
+
+            # Accumulate c1 and c2 sums:
+
+            x_row_ptr = X + row * stride
+            dy_row_ptr = DY + row * stride
+
+            c1 = 0.0
+            c2 = 0.0
+
+            for block_idx in tl.range(0, num_col_blocks):
+                cols = block_idx * BLOCK_SIZE_N + col_offsets
+
+                x = tl.load(x_row_ptr + cols).to(tl.float32)
+                dy = tl.load(dy_row_ptr + cols).to(tl.float32)
+                w = tl.load(W + cols).to(tl.float32)
+
+                xhat = (x - mean) * rstd
+                wdy = w * dy
+                c1 += tl.sum(xhat * wdy, axis=0)
+                c2 += tl.sum(wdy, axis=0)
+
+            cols = num_col_blocks * BLOCK_SIZE_N + col_offsets
+            mask = cols < N
+
+            x = tl.load(x_row_ptr + cols, mask=mask, other=0).to(tl.float32)
+            dy = tl.load(dy_row_ptr + cols, mask=mask, other=0).to(tl.float32)
+            w = tl.load(W + cols, mask=mask, other=0).to(tl.float32)
+
+            xhat = (x - mean) * rstd
+            wdy = w * dy
+            wdy = tl.where(mask, wdy, 0)
+            c1 += tl.sum(xhat * wdy, axis=0)
+            c2 += tl.sum(wdy, axis=0)
+
+            c1 /= N
+            c2 /= N
+
+            # Compute dx and partial sums for dw and db:
+
+            dx_row_ptr = DX + row * stride
+            if not IGNORE_DW_DB:
+                dw_row_ptr = DW + pid * N
+                db_row_ptr = DB + pid * N
+
+            for block_idx in tl.range(0, num_col_blocks):
+                cols = block_idx * BLOCK_SIZE_N + col_offsets
+
+                x = tl.load(x_row_ptr + cols).to(tl.float32)
+                dy = tl.load(dy_row_ptr + cols).to(tl.float32)
+                w = tl.load(W + cols).to(tl.float32)
+
+                xhat = (x - mean) * rstd
+                wdy = w * dy
+
+                dx = (wdy - (xhat * c1 + c2)) * rstd
+                tl.store(dx_row_ptr + cols, dx.to(DX.type.element_ty))
+                if not IGNORE_DW_DB:
+                    partial_dw = dy * xhat
+                    dw_ptrs = dw_row_ptr + cols
+                    partial_dw += tl.load(dw_ptrs).to(tl.float32)
+                    tl.store(dw_ptrs, partial_dw.to(DW.type.element_ty))
+
+                    partial_db = dy
+                    db_ptrs = db_row_ptr + cols
+                    partial_db += tl.load(db_ptrs).to(tl.float32)
+                    tl.store(db_ptrs, partial_db.to(DB.type.element_ty))
+
+            cols = num_col_blocks * BLOCK_SIZE_N + col_offsets
+            mask = cols < N
+
+            x = tl.load(x_row_ptr + cols, mask=mask, other=0).to(tl.float32)
+            dy = tl.load(dy_row_ptr + cols, mask=mask, other=0).to(tl.float32)
+            w = tl.load(W + cols, mask=mask, other=0).to(tl.float32)
+
+            xhat = (x - mean) * rstd
+            wdy = w * dy
+
+            dx = (wdy - (xhat * c1 + c2)) * rstd
+            tl.store(dx_row_ptr + cols, dx.to(DX.type.element_ty), mask=mask)
+            if not IGNORE_DW_DB:
+                partial_dw = dy * xhat
+                dw_ptrs = dw_row_ptr + cols
+                partial_dw += tl.load(dw_ptrs, mask=mask).to(tl.float32)
+                tl.store(dw_ptrs, partial_dw.to(DW.type.element_ty), mask=mask)
+
+                partial_db = dy
+                db_ptrs = db_row_ptr + cols
+                partial_db += tl.load(db_ptrs, mask=mask).to(tl.float32)
+                tl.store(db_ptrs, partial_db.to(DB.type.element_ty), mask=mask)
+
+            # Advance to next row.
+            row += tile_num
+
     else:
-        return torch.iinfo(dtype).max
+        # Unblocked approach:
+
+        cols = tl.arange(0, BLOCK_SIZE_N)
+        mask = cols < N
+        row = pid
+        if not IGNORE_DW_DB:
+            dw_row = tl.zeros((BLOCK_SIZE_N,), dtype=tl.float32)
+            db_row = tl.zeros((BLOCK_SIZE_N,), dtype=tl.float32)
+
+        for _ in range(0, rows_per_tile):
+            # Compute pointers:
+            x_ptrs = X + row * stride
+            dy_ptrs = DY + row * stride
+            dx_ptrs = DX + row * stride
+
+            # Load data to SRAM:
+            x = tl.load(x_ptrs + cols, mask=mask, other=0).to(tl.float32)
+            dy = tl.load(dy_ptrs + cols, mask=mask, other=0).to(tl.float32)
+            w = tl.load(W + cols, mask=mask, other=0).to(tl.float32)
+            mean = tl.load(Mean + row)
+            rstd = tl.load(Rstd + row)
+
+            # Compute dx:
+            xhat = (x - mean) * rstd
+            wdy = w * dy
+            wdy = tl.where(mask, wdy, 0)
+            c1 = tl.sum(xhat * wdy, axis=0) / N
+            c2 = tl.sum(wdy, axis=0) / N
+            dx = (wdy - (xhat * c1 + c2)) * rstd
+
+            # Write dx:
+            tl.store(dx_ptrs + cols, dx.to(DX.type.element_ty), mask=mask)
+            if not IGNORE_DW_DB:
+                # Accumulate partial sums for dw and db:
+                dw_row += dy * xhat
+                db_row += dy
+
+            # Advance to next row:
+            row += tile_num
+        if not IGNORE_DW_DB:
+            tl.store(DW + pid * N + cols, dw_row.to(DW.type.element_ty), mask=mask)
+            tl.store(DB + pid * N + cols, db_row.to(DB.type.element_ty), mask=mask)
+
+
+@triton.jit
+def _layernorm_bwd_dwdb_triton(
+    DW,  # pointer to the partial sum of weights gradient
+    DB,  # pointer to the partial sum of biases gradient
+    FINAL_DW,  # pointer to the weights gradient
+    FINAL_DB,  # pointer to the biases gradient
+    M,  # GROUP_SIZE_M
+    N,  # number of columns
+    BLOCK_SIZE_M: tl.constexpr,
+    BLOCK_SIZE_N: tl.constexpr,
+):
+    # Map the program id to the elements of DW and DB it should compute.
+    pid = tl.program_id(0)
+    cols = pid * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+    dw = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+    db = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+    # Iterate through the rows of DW and DB to sum the partial sums.
+    for i in range(0, M, BLOCK_SIZE_M):
+        rows = i + tl.arange(0, BLOCK_SIZE_M)
+        mask = (rows[:, None] < M) & (cols[None, :] < N)
+        offs = rows[:, None] * N + cols[None, :]
+        dw += tl.load(DW + offs, mask=mask, other=0.0)
+        db += tl.load(DB + offs, mask=mask, other=0.0)
+    # Write the final sum to the output.
+    sum_dw = tl.sum(dw, axis=0)
+    sum_db = tl.sum(db, axis=0)
+    tl.store(FINAL_DW + cols, sum_dw.to(FINAL_DW.type.element_ty), mask=cols < N)
+    tl.store(FINAL_DB + cols, sum_db.to(FINAL_DB.type.element_ty), mask=cols < N)
+
+
+@triton.jit
+def _layernorm_bwd_dwdb_triton_v2(
+    X,  # pointer to the input
+    DY,  # pointer to the output gradient
+    Mean,  # pointer to the mean
+    Rstd,  # pointer to the 1/std
+    stride,
+    FINAL_DW,  # pointer to the weights gradient
+    FINAL_DB,  # pointer to the biases gradient
+    M,  # GROUP_SIZE_M
+    N,  # number of columns
+    BLOCK_SIZE_M: tl.constexpr,
+    BLOCK_SIZE_N: tl.constexpr,
+):
+    pid = tl.program_id(0)
+    cols = pid * BLOCK_SIZE_N + tl.arange(0, BLOCK_SIZE_N)
+    dw = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+    db = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+    # Iterate through the rows of x and dy to compute dw and db
+    for i in range(0, M, BLOCK_SIZE_M):
+        rows = i + tl.arange(0, BLOCK_SIZE_M)
+        means = tl.load(Mean + rows, mask=rows < M, other=0.0).to(tl.float32)
+        rstds = tl.load(Rstd + rows, mask=rows < M, other=0.0).to(tl.float32)
+        mask = (rows[:, None] < M) & (cols[None, :] < N)
+        offs = rows[:, None] * stride + cols[None, :]
+        x = tl.load(X + offs, mask=mask, other=0.0).to(tl.float32)
+        dy = tl.load(DY + offs, mask=mask, other=0.0).to(tl.float32)
+        xhat = (x - means[:, None]) * rstds[:, None]
+        dw += dy * xhat
+        db += dy
+    # Write the final sum to the output.
+    sum_dw = tl.sum(dw, axis=0)
+    sum_db = tl.sum(db, axis=0)
+    tl.store(FINAL_DW + cols, sum_dw.to(FINAL_DW.type.element_ty), mask=cols < N)
+    tl.store(FINAL_DB + cols, sum_db.to(FINAL_DB.type.element_ty), mask=cols < N)
+
+
+def _layernorm_backward(
+    dy: torch.Tensor,
+    dx: torch.Tensor,
+    dw: torch.Tensor,
+    db: torch.Tensor,
+    x: torch.Tensor,
+    gamma: torch.Tensor,
+    mu: torch.Tensor,
+    rsigma: torch.Tensor,
+):
+
+    M, N = x.shape
+    # calculate dw and db separately when M is small
+    IGNORE_DW_DB_IN_FUSED = M <= 512
+    tile_num = max(min(256, M // 4), 1)
+    if M <= 512 and M * N < 64 * 1024 * 1024:
+        tile_num = M
+    elif M >= 8192:
+        tile_num = 2048
+    max_fused_size = 32768 // x.element_size()
+    next_power = triton.next_power_of_2(N)
+    BLOCK_SIZE = min(max_fused_size, next_power)
+    # For cases with small M and large N, decrease block size to help with occupancy and register spill
+    if tile_num == M:
+        if tile_num > 256:
+            BLOCK_SIZE = min(BLOCK_SIZE, 2048)
+        else:
+            BLOCK_SIZE = min(BLOCK_SIZE, 4096)
+    USE_BLOCKED = N > BLOCK_SIZE
+    num_warps = min(max(BLOCK_SIZE // 256, 1), 8)
+
+    if not IGNORE_DW_DB_IN_FUSED:
+        _dw = torch.zeros((tile_num, N), dtype=torch.float32, device=gamma.device)
+        _db = torch.zeros((tile_num, N), dtype=torch.float32, device=gamma.device)
+    else:
+        _dw = None
+        _db = None
+
+    grid_bwd = (tile_num,)
+    _layernorm_bwd_dx_fused_triton[grid_bwd](
+        dx,
+        dy,
+        _dw,
+        _db,
+        x,
+        gamma,
+        mu,
+        rsigma,
+        x.stride(0),
+        N,
+        NUM_ROWS=M,
+        BLOCK_SIZE_N=BLOCK_SIZE,
+        USE_BLOCKED=USE_BLOCKED,
+        num_warps=num_warps,
+        IGNORE_DW_DB=IGNORE_DW_DB_IN_FUSED,
+    )
+    grid_reduce = lambda meta: (triton.cdiv(N, meta["BLOCK_SIZE_N"]),)  # noqa: E731
+    if not IGNORE_DW_DB_IN_FUSED:
+        dwdb_block_n = max(16, N // 256)
+        dwdb_block_n = triton.next_power_of_2(dwdb_block_n)
+        dwdb_block_m = (64 * 128) // dwdb_block_n
+        dwdb_block_m = min(triton.next_power_of_2(tile_num), dwdb_block_m)
+        _layernorm_bwd_dwdb_triton[grid_reduce](
+            _dw,
+            _db,
+            dw,
+            db,
+            min(tile_num, M),
+            N,
+            BLOCK_SIZE_M=dwdb_block_m,
+            BLOCK_SIZE_N=dwdb_block_n,
+        )
+    else:
+        dwdb_block_n = max(16, N // 256)
+        dwdb_block_n = triton.next_power_of_2(dwdb_block_n)
+        dwdb_block_m = (64 * 128) // dwdb_block_n
+        dwdb_block_m = min(triton.next_power_of_2(M), dwdb_block_m)
+        _layernorm_bwd_dwdb_triton_v2[grid_reduce](
+            x,
+            dy,
+            mu,
+            rsigma,
+            x.stride(0),
+            dw,
+            db,
+            M,
+            N,
+            BLOCK_SIZE_M=dwdb_block_m,
+            BLOCK_SIZE_N=dwdb_block_n,
+        )
+
+    return dx, dw, db
+
+
+class _LayerNorm(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, x, weight, bias, eps, is_grad_enabled):
+
+        is_grad = is_grad_enabled and any(
+            tensor.requires_grad for tensor in [x, weight, bias]
+        )
+
+        y = torch.empty_like(x)
+        M = x.shape[0]
+        mean = torch.empty((M,), dtype=torch.float32, device=x.device)
+        rstd = torch.empty((M,), dtype=torch.float32, device=x.device)
+
+        _layernorm_forward(y, x, weight, bias, mean, rstd, eps)
+
+        if is_grad:
+            ctx.save_for_backward(x, weight, mean, rstd)
+
+        return y
+
+    @staticmethod
+    def backward(ctx, dy):
+        x, w, m, v = ctx.saved_tensors
+        N = w.shape[0]
+
+        dw = torch.empty((N,), dtype=w.dtype, device=w.device)
+        db = torch.empty((N,), dtype=w.dtype, device=w.device)
+        dx = torch.empty_like(dy)
+
+        _layernorm_backward(dy, dx, dw, db, x, w, m, v)
+
+        return dx, dw, db, None, None
+
+
+class _Layernorm2dFwdWithAdd(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx, y, x, res_in, res_out, weight, bias, eps, is_grad_enabled):
+
+        is_grad = is_grad_enabled and any(
+            tensor.requires_grad for tensor in [x, weight, bias]
+        )
+
+        M = x.shape[0]
+        mean = torch.empty((M,), dtype=torch.float32, device=x.device)
+        rstd = torch.empty((M,), dtype=torch.float32, device=x.device)
+
+        _layernorm_forward_with_add(
+            y, x, res_in, res_out, weight, bias, mean, rstd, eps
+        )
+
+        if is_grad:
+            ctx.save_for_backward(res_out, weight, mean, rstd)
+
+        return y
+
+    @staticmethod
+    def backward(ctx, dy):
+        x, w, m, v = ctx.saved_tensors
+        N = w.shape[0]
+
+        dw = torch.empty((N,), dtype=w.dtype, device=w.device)
+        db = torch.empty((N,), dtype=w.dtype, device=w.device)
+        dx = torch.empty_like(dy)
+
+        _layernorm_backward(dy, dx, dw, db, x, w, m, v)
+
+        return None, dx, None, None, dw, db, None, None
 
 
 def layer_norm(
@@ -597,18 +1066,7 @@ def layer_norm(
     Returns:
     - Output: The output tensor with shape (M, N).
     """
-    out = torch.empty_like(input)
-    M, N = input.shape
-
-    # Less than 64KB per feature: enqueue fused kernel
-    MAX_FUSED_SIZE = 65536 // input.element_size()
-    BLOCK_SIZE = min(MAX_FUSED_SIZE, triton.next_power_of_2(N))
-
-    _layernorm_kernel[(M,)](
-        input, out, weight, bias, input.stride(0), out.stride(0), M, N, eps, BLOCK_SIZE
-    )
-
-    return out
+    return _LayerNorm.apply(input, weight, bias, eps, torch.is_grad_enabled())
 
 
 def layernorm2d_fwd_with_add(
@@ -631,34 +1089,22 @@ def layernorm2d_fwd_with_add(
     - residual_out: Output tensor that is input + residual_in with shape (M, N). Must be allocated by the caller
     - weight: The learnable weights tensor with shape (N, ).
     - bias: Bias added to the result of layer norm with shape (N,)
-    - eps: A value added to the denominator for numerical stability.
+    - epsilon: A value added to the denominator for numerical stability.
 
     Returns:
     - out: The output tensor with shape (M, N).
     - residual_out: Output tensor that is input + residual_in with shape (M, N).
     """
-    M, N = input.shape
-
-    # Less than 64KB per feature: enqueue fused kernel
-    MAX_FUSED_SIZE = 65536 // input.element_size()
-    BLOCK_SIZE = min(MAX_FUSED_SIZE, triton.next_power_of_2(N))
-
-    _fused_add_layernorm_kernel[(M,)](
-        input,
+    return _Layernorm2dFwdWithAdd.apply(
         out,
+        input,
         residual_in,
         residual_out,
         weight,
         bias,
-        input.stride(0),
-        out.stride(0),
-        M,
-        N,
         epsilon,
-        BLOCK_SIZE,
+        torch.is_grad_enabled(),
     )
-
-    return
 
 
 def layernorm2d_fwd_with_dynamicquant(
@@ -717,7 +1163,7 @@ def layernorm2d_fwd_with_dynamicquant(
         BLOCK_SIZE,
     )
 
-    return out
+    return
 
 
 def layernorm2d_fwd_with_smoothquant(
@@ -775,7 +1221,7 @@ def layernorm2d_fwd_with_smoothquant(
         BLOCK_SIZE,
     )
 
-    return out
+    return
 
 
 def layernorm2d_fwd_with_add_dynamicquant(
@@ -840,7 +1286,7 @@ def layernorm2d_fwd_with_add_dynamicquant(
         BLOCK_SIZE,
     )
 
-    return out
+    return
 
 
 def layernorm2d_fwd_with_add_smoothquant(
@@ -906,4 +1352,4 @@ def layernorm2d_fwd_with_add_smoothquant(
         BLOCK_SIZE,
     )
 
-    return out
+    return
--- op_tests/triton_tests/test_layernorm.py
+++ op_tests/triton_tests/test_layernorm.py
@@ -65,7 +65,7 @@ def run_triton(
         else:
             residual_out = torch.empty_like(input)
             output = torch.empty_like(input)
-            layernorm2d_fwd_with_add(
+            output = layernorm2d_fwd_with_add(
                 output, input, residual, residual_out, weight, bias, eps, x_bias
             )
     elif x_scale is None:
@@ -137,12 +137,24 @@ def test_layernorm(M, N, dtype_str, eps=1e-5):
     torch.manual_seed(0)
     x = torch.randn(M, N, device="cuda", dtype=dtype)
     w_shape = (N,)
-    b = torch.rand(w_shape, device="cuda", dtype=dtype)
-    w = torch.rand(w_shape, device="cuda", dtype=dtype)
+    b = torch.rand(w_shape, dtype=dtype, device="cuda", requires_grad=True)
+    w = torch.rand(w_shape, dtype=dtype, device="cuda", requires_grad=True)
+
+    dy = 0.1 * torch.randn_like(x)
+    x.requires_grad_(True)
 
     # forward pass
     y_torch, *_ = run_torch(x, w, b, eps)
     y_triton, *_ = run_triton(x, w, b, eps)
+
+    # backward pass (triton)
+    y_triton.backward(dy, retain_graph=True)
+    dx_triton, dw_triton, db_triton = [_.grad.clone() for _ in [x, w, b]]
+    x.grad, w.grad, b.grad = None, None, None
+
+    # backward pass (torch)
+    y_torch.backward(dy, retain_graph=True)
+    dx_torch, dw_torch, db_torch = [_.grad.clone() for _ in [x, w, b]]
 
     if dtype in (torch.float16, torch.bfloat16):
         atol, rtol = 1e-2, 1e-2
@@ -151,6 +163,9 @@ def test_layernorm(M, N, dtype_str, eps=1e-5):
         atol, rtol = 1e-5, 1e-5
 
     triton.testing.assert_close(y_triton, y_torch, atol=atol, rtol=rtol)
+    triton.testing.assert_close(dx_triton, dx_torch, rtol=rtol, atol=atol)
+    triton.testing.assert_close(db_triton, db_torch, rtol=rtol, atol=atol)
+    triton.testing.assert_close(dw_triton, dw_torch, rtol=rtol, atol=atol)
 
 
 # pytest
@@ -165,12 +180,24 @@ def test_fused_add_layernorm(M, N, dtype_str, eps=1e-5):
     x = torch.randn(M, N, device="cuda", dtype=dtype)
     res = torch.randn(M, N, device="cuda", dtype=dtype)
     w_shape = (N,)
-    w = torch.rand(w_shape, device="cuda", dtype=dtype)
-    b = torch.rand(w_shape, device="cuda", dtype=dtype)
+    b = torch.rand(w_shape, dtype=dtype, device="cuda", requires_grad=True)
+    w = torch.rand(w_shape, dtype=dtype, device="cuda", requires_grad=True)
+
+    dy = 0.1 * torch.randn_like(x)
+    x.requires_grad_(True)
 
     # forward pass
     y_torch, res_torch, *_ = run_torch(x, w, b, eps, residual=res)
     y_triton, res_triton, *_ = run_triton(x, w, b, eps, residual=res)
+
+    # backward pass (triton)
+    y_triton.backward(dy, retain_graph=True)
+    dx_triton, dw_triton, db_triton = [_.grad.clone() for _ in [x, w, b]]
+    x.grad, w.grad, b.grad = None, None, None
+
+    # backward pass (torch)
+    y_torch.backward(dy, retain_graph=True)
+    dx_torch, dw_torch, db_torch = [_.grad.clone() for _ in [x, w, b]]
 
     if dtype in (torch.float16, torch.bfloat16):
         atol, rtol = 1e-2, 1e-2
@@ -180,6 +207,9 @@ def test_fused_add_layernorm(M, N, dtype_str, eps=1e-5):
 
     triton.testing.assert_close(y_triton, y_torch, atol=atol, rtol=rtol)
     triton.testing.assert_close(res_triton, res_torch, atol=atol, rtol=rtol)
+    triton.testing.assert_close(dx_triton, dx_torch, rtol=rtol, atol=atol)
+    triton.testing.assert_close(db_triton, db_torch, rtol=rtol, atol=atol)
+    triton.testing.assert_close(dw_triton, dw_torch, rtol=rtol, atol=atol)
 
 
 @pytest.mark.parametrize("dtype_str", ["fp32", "fp16", "bf16"])

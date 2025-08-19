53a412a39c3262445b5d6fa89ce10431447d3592
slippedJim <jim.guo@amd.com>
2025-08-19 20:27:05 +0800

    fix codegen import path (#845)

--- csrc/cpp_itfs/mha_fwd_generate.py
+++ csrc/cpp_itfs/mha_fwd_generate.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from typing import Optional
 
 this_dir = os.path.dirname(os.path.abspath(__file__))
-AITER_CORE_DIR = os.path.abspath(f"{this_dir}/../../")
+AITER_CORE_DIR = os.path.abspath(f"{this_dir}/../../../")
 if os.path.exists(os.path.join(AITER_CORE_DIR, "aiter_meta")):
     AITER_CORE_DIR = os.path.join(AITER_CORE_DIR, "aiter/jit/utils")  # pip install mode
 else:
